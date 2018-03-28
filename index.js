b69f77d3725f027300b07afbfeed8042dfc9fc58
dotzipfile <davenickmalone@gmail.com>
2018-03-28 23:54:14 +0100

    added some background processes to perform checks

--- index.js
+++ index.js
@@ -5,20 +5,18 @@
 
 // Dependencies
 var server = require('./lib/server');
-//var workers = require('./lib/workers');
+var workers = require('./lib/workers');
 
 // Declare the app
 var app = {};
 
 // Init function
 app.init = function(){
-
   // Start the server
   server.init();
 
   // Start the workers
-  //workers.init();
-
+  workers.init();
 };
 
 // Self executing
